44694e3e70dfb1d45026278144847f4d762a22c2
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-12-30 12:14:29 +0700

    up

--- example/change_color/ios/Flutter/flutter_export_environment.sh
+++ example/change_color/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/isan/applications/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/isan/code/flutter/me/Flare-Flutter/example/guss"
+export "FLUTTER_APPLICATION_PATH=/home/isan/code/flutter/me/Flare-Flutter/example/change_color"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
--- example/guss/lib/main.dart
+++ example/guss/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flare_flutter/flare_actor.dart';
+// import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
